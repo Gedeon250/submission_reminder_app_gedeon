d49c5214c7930b897ee918c80d47a79636d4754e
Gedeon Ntigibeshya <g.ntigibesh@alustudent.com>
2025-02-10 18:29:39 +0200

    create_environment.sh

--- create_environment.sh
+++ create_environment.sh
@@ -9,13 +9,5 @@ APP_DIR="submission_reminder_${user_name}"
 # Create the main application directory
 mkdir -p "$APP_DIR"
 
-# Create the submissions.txt file inside the directory
-echo -e "Student, Assignment, Submission Status" > "$APP_DIR/submissions.txt"
-echo -e "Chinemerem, Shell Navigation, not submitted" >> "$APP_DIR/submissions.txt"
-echo -e "Chiagoziem, Git, submitted" >> "$APP_DIR/submissions.txt"
-echo -e "Divine, Shell Navigation, not submitted" >> "$APP_DIR/submissions.txt"
-echo -e "Anissa, Shell Basics, submitted" >> "$APP_DIR/submissions.txt"
-
 # Confirmation message
 echo "Directory and file created successfully!"
-echo "Check inside $APP_DIR for submissions.txt."
